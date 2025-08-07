50b8146d81f76eaf39043b53e2353c78316cb240
jonathon-hackbarth <jonathon.hackbarth@gmail.com>
2025-08-07 16:07:09 -0500

    added sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -2,17 +2,17 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://jonathonhackbarth.com",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   output: "server",
   adapter: vercel({
     imageService: true,
-    webAnalytics: {
-      enabled: true,
-    },
+    webAnalytics: { enabled: true },
   }),
 });
